b43faed2946eeb7fa95dd66c76fdbdd7d460d678
TMRR-Code <154716205+TMRR-Code@users.noreply.github.com>
2024-03-03 17:52:14 +0700

    Update client.py

--- communication/client.py
+++ communication/client.py
@@ -27,13 +27,5 @@ while True:
     response = send_message(message)
     print("Server:", response)
 
-# import socket
-# host = socket.gethostname()
-# port = 12345
-# s = socket.socket()
-# s.connect((host, port))
-# s.sendall(b'Halo nama Saya TMRR!')
-# data = s.recv(1024)
-# s.close()
-# print(repr(data))
+
 
